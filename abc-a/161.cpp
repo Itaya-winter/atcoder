d0ce6a9fe2c837e6b1c8cc4029d5a49ffecabf5e
Itaya-winter <is0454hx@ed.ritsumei.ac.jp>
2020-04-09 14:57:26 +0900

    fix:wa at 161a

--- abc-a/161.cpp
+++ abc-a/161.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 int main (){
     int x,y,z;
-    cin >> x >> y>>z;
-    cout << z << x << y << endl;
+    cin >> x >> y>> z;
+    cout << z <<" "<< x <<" "<< y << endl;
 
     return 0;
 }
