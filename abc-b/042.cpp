c69be16f8cb714a70858621e6ce86567c02cf846
Itaya-winter <is0454hx@ed.ritsumei.ac.jp>
2020-04-09 18:28:05 +0900

    solve:042b

---
+++ abc-b/042.cpp
@@ -0,0 +1,20 @@
+#define ll long long
+#define INF 99999999
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+int l,n;
+cin >> n >> l;
+vector<string> s(n);
+rep(i,n){
+    cin >> s[i];
+}
+sort(s.begin(),s.end());
+rep(i,n){
+    cout<< s[i] ;
+}
+return 0;
+
+}
