a163c4b14dfa54876859978efe6972304229ea55
Itaya-winter <is0454hx@ed.ritsumei.ac.jp>
2020-04-09 15:18:44 +0900

    solve:151a

---
+++ abc-a/151.cpp
@@ -0,0 +1,12 @@
+#define ll long long
+#define INF 99999999
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char c;
+    cin >> c;
+    c++ ;
+    cout << c; 
+    return 0;
+}
