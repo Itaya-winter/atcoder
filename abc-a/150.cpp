92f4e6c7c82698fee4b2a0f437ec6df2dd895b1a
Itaya-winter <is0454hx@ed.ritsumei.ac.jp>
2020-04-09 15:31:33 +0900

    solve:150a

---
+++ abc-a/150.cpp
@@ -0,0 +1,13 @@
+#define ll long long
+#define INF 99999999
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int k, x;
+    cin >> k >> x;
+    if(x<=k*500)cout << "Yes";
+    else cout << "No";
+
+    return 0;
+}
