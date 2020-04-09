b47fe4534fb3eacca458961b770b5a25f0188f5a
Itaya-winter <is0454hx@ed.ritsumei.ac.jp>
2020-04-09 14:53:36 +0900

    161a

---
+++ abc-a/161.cpp
@@ -0,0 +1,13 @@
+#define ll long long
+#define INF 99999999
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    int x,y,z;
+    cin >> x >> y>>z;
+    cout << z << x << y << endl;
+
+    return 0;
+}
