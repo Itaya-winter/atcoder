46d03a1a329c3592790c875349f67b87dc02f319
Itaya-winter <is0454hx@ed.ritsumei.ac.jp>
2020-04-10 14:44:35 +0900

    solve:140a

---
+++ abc-a/140.cpp
@@ -0,0 +1,11 @@
+#define ll long long
+#define INF 99999999
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int r;
+    cin >> r;
+    cout << r*r*r;
+    return 0;
+}
