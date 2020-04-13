e5f7e0852caf42afb957da1076b8439d7ad9bd54
Itaya-winter <is0454hx@ed.ritsumei.ac.jp>
2020-04-13 22:06:22 +0900

    solve:b

---
+++ パナソニックプログラミングコンテスト2020/b.cpp
@@ -0,0 +1,13 @@
+#define ll long long
+#define INF 99999999
+#define rep(i, n) for(int i = 0; i < (int)(n); i++)
+#include<bits/stdc++.h>
+using namespace std;
+
+int main (){
+    ll a,b;
+    cin >> a >> b;
+    cout  << (ll)(a*b+1)/2 << endl;
+    return 0;
+
+}
